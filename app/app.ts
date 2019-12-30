95cd1f19507c2b5a54c44d45f5b26a4e3e9e2c3b
Kamal Lamgade <kamal.lamgade@n4l.co.nz>
2019-12-31 10:03:07 +1300

    sample arrow function: es2015

--- app/app.ts
+++ app/app.ts
@@ -1,14 +1,21 @@
 
+//arrow function es2015 demo
+let squareIt = y => y * y;
 
+function arrowFuncTest(value: number): number {
+    let result: number  = squareIt(value);
+    return result;
+ }
+ console.log("arrow func result : " + arrowFuncTest(9));
+
+ //working with html element
 document.getElementById('startGame').addEventListener('click', startGame);
 
-function startGame(x: any = 2): void{
+function startGame(x: any = 2): void {
     //union types: --strictNullChecks
     let playerName: string | null | undefined = "dalli girl";
     console.log(playerName + " x value:  " + x);
-    var messageElement : HTMLElement ;
+    var messageElement: HTMLElement;
     messageElement = document.getElementById('messages');
     messageElement.innerText = " Welcome!!"
 }
-
-startGame();
